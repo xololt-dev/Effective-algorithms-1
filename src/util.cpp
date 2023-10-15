dfcf6fc49f7a2cf04f2286a7ebc4712e29cdbb5e
Xololt <105434979+xololt-dev@users.noreply.github.com>
2023-10-15 20:40:13 +0200

    Data generation function added loadFromFile data input refactor

--- src/util.cpp
+++ src/util.cpp
@@ -7,6 +7,9 @@ void Matrix::loadFromFile(std::string fileName) {
 	file.open(fileName, std::ios::in);
 
 	if (file.good()) {
+		// jeœli istnieje poprzednia matryca, czyœcimy
+		if (mat.size()) mat.clear();
+
 		int dimension = 0, cross = 0, valueInMatrix = 0;
 		std::string stringTemp;
 
@@ -18,16 +21,9 @@ void Matrix::loadFromFile(std::string fileName) {
 		std::cout << stringTemp << std::endl;
 		file >> dimension;
 
-		this->size = dimension;
+		this->size = dimension;		
 		this->mat.reserve(dimension);
-		std::vector<std::vector<int>>::iterator matIter;
-		
-		/*
-		* Nie wiem czy ten reserve jest dobrze zrobiony - i tak nie jest to kluczowe
-		for (matIter = mat.begin(); matIter != mat.end(); matIter++) {
-			(*matIter).reserve(dimension);
-		}
-		*/
+		std::vector<std::vector<int>>::iterator matIter = mat.begin();
 
 		std::cout << dimension << std::endl << std::endl;
 
@@ -36,64 +32,68 @@ void Matrix::loadFromFile(std::string fileName) {
 		} while (stringTemp != "EDGE_WEIGHT_SECTION");
 		
 		// Dotarcie do wartoœci zapisanych w matrycy, podgl¹d wartoœci zapisanych na przek¹tnych (?)
-		// std::cout << stringTemp << std::endl;
 		file >> stringTemp;
 		cross = std::stoi(stringTemp);
-		
-		matIter = mat.begin();
 
-		while (true) {
-			file >> stringTemp;
-			if (stringTemp == "EOF") break;
-			
+		while (stringTemp != "EOF") {
 			valueInMatrix = std::stoi(stringTemp);
 
-			// czasami przek¹tne s¹ mieszane miêdzy wartoœci¹ cross i 0
-			if (valueInMatrix == cross || valueInMatrix == 0) {
+			// pierwszy wiersz wymaga specjalnego traktowania - musimy zrobiæ push_back vectorów
+			if (mat.size() < size) {
+				std::vector<int> tempVec;
+				tempVec.push_back(!mat.size() ? 0 : valueInMatrix);
+				mat.push_back(tempVec);
+
 				matIter = mat.begin();
-				// std::cout << "\n";
 			}
+			// reszta normalny pushback
 			else {
-				// pierwszy wiersz wymaga specjalnego traktowania - musimy zrobiæ push_back vectorów
-				if (mat.size() < size) {
-					std::vector<int> tempVec;
-					tempVec.push_back(valueInMatrix);
-					mat.push_back(tempVec);
-					matIter = mat.begin();
-				}
-				// reszta normalny pushback
-				else {
-					(*matIter).push_back(valueInMatrix);
-					matIter++;
-				}
-				/*
-				int spacesToAdd = 2 - (int)log10((double)valueInMatrix);
-				std::cout << stringTemp << " ";
+				if (valueInMatrix == cross || valueInMatrix == 0) (*matIter).push_back(0);
+				else (*matIter).push_back(valueInMatrix);
 
-				while (spacesToAdd > 0) {
-					std::cout << " ";
-					spacesToAdd--;
-				}*/
+				matIter++;
+				if (matIter == mat.end()) matIter = mat.begin();
 			}
-		}
 
+			file >> stringTemp;
+		}
 		file.close();
 	}
 	else std::cout << "Plik nie zostal otworzony!\n";
+}
+
+void Matrix::generate(int size) {
+	if (mat.size()) mat.clear();
+	srand(time(NULL));
+	mat.reserve(size);
+	this->size = size;
+
+	for (int i = 0; i < size; i++) {
+		std::vector<int> temp;
+		temp.reserve(size);
+		for (int j = 0; j < size; j++) {
+			if (i == j) temp.push_back(0);
+			else temp.push_back(rand() % 1000);
+		}
+		mat.push_back(temp);
+	}
 }
 
 void Matrix::display() {
 	std::vector<std::vector<int>>::iterator matIter;
 	std::vector<int>::iterator matIterInner;
 
-	for (int i = 0; i < mat.size() - 1; i++) {
+	for (int i = 0; i < mat.size(); i++) {
 		for (matIter = mat.begin(); matIter != mat.end(); matIter++) {
 			matIterInner = (*matIter).begin();
 			std::advance(matIterInner, i);
 
-			int spacesToAdd = 2 - (int)log10((double)(*matIterInner));
-			std::cout << (*matIterInner) << " ";
+			int spacesToAdd = 2;
+			if (*matIterInner) {
+				spacesToAdd -= (int)log10((double)(*matIterInner));
+			}
 
+			std::cout << (*matIterInner) << " ";
 			while (spacesToAdd > 0) {
 				std::cout << " ";
 				spacesToAdd--;
@@ -101,4 +101,12 @@ void Matrix::display() {
 		}
 		std::cout << "\n";
 	}	
+}
+
+void clear() {
+#if defined _WIN32
+	system("cls");
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+	system("clear");
+#endif
 }

f8c7b89c7327f0cb4bfda23eb2ff742551a14f40
Xololt <105434979+xololt-dev@users.noreply.github.com>
2023-10-24 10:29:52 +0200

    General brute force algorithm cleanup, added an option to choose between single and multithreaded option Added helper function to display results in a prettier way Added early termination for ST version

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 #include <thread>
 
-void Algorithms::bruteForce(Matrix* matrix) {
+void Algorithms::bruteForce(Matrix* matrix, int multithread) {
 	std::chrono::time_point<std::chrono::steady_clock> now = std::chrono::steady_clock::now();
 	
-	if (1) {
-		int maxThreadsPossible = std::thread::hardware_concurrency();
+	if (multithread) {
+		// int maxThreadsPossible = std::thread::hardware_concurrency();
 
 		std::vector<std::vector<int>> orders; // results dump
 		std::vector<int> pathLengths; // results dump
@@ -19,105 +19,91 @@ void Algorithms::bruteForce(Matrix* matrix) {
 		int previousVertex = 1;
 
 		// mo¿na stworzyæ wartoœci na ka¿dy w¹tek bez problemu 
-		if (/*matrixSize <= maxThreadsPossible && */matrixSize > 3) {
-			std::vector<std::thread*> vectorOfThreadsInFlight;
-			vectorOfThreadsInFlight.reserve(matrixSize - 1);
-			orders.resize(matrixSize - 1);
-			pathLengths.resize(matrixSize - 1, INT_MAX);
-			std::vector<std::vector<int>>::iterator ordersIterator = orders.begin();
-			std::vector<int>::iterator pathLengthsIterator = pathLengths.begin();
+		
+		std::vector<std::thread*> vectorOfThreadsInFlight;
+		vectorOfThreadsInFlight.reserve(matrixSize - 1);
+		orders.resize(matrixSize - 1);
+		pathLengths.resize(matrixSize - 1, INT_MAX);
+		std::vector<std::vector<int>>::iterator ordersIterator = orders.begin();
+		std::vector<int>::iterator pathLengthsIterator = pathLengths.begin();
 
-			do {
-				// czyœcimy passOff, wype³niamy od nowa
-				if (previousVertex != permutationVector.front()) {
-					vectorOfThreadsInFlight.push_back(new std::thread(bruteHelperMultithreadNew,
-						&*ordersIterator,
-						&*pathLengthsIterator,
-						permutationVector,
-						permutationVector.front(),
-						matrix)
-					);
-					vectorOfThreadsInFlight.back()->detach();
+		// tworzenie w¹tków dla zestawów permutacji
+		do {
+			// czyœcimy passOff, wype³niamy od nowa
+			if (previousVertex != permutationVector.front()) {
+				vectorOfThreadsInFlight.push_back(new std::thread(bruteHelperMultithread,
+					&*ordersIterator,
+					&*pathLengthsIterator,
+					permutationVector,
+					permutationVector.front(),
+					matrix)
+				);
+				vectorOfThreadsInFlight.back()->detach();
 
-					ordersIterator++;
-					pathLengthsIterator++;
-					previousVertex = permutationVector.front();
-				}
-			} while (std::next_permutation(permutationVector.begin(), permutationVector.end()));
-
-			vectorOfThreadsInFlight.push_back(new std::thread(bruteHelperMultithreadNew,
-				&*ordersIterator,
-				&*pathLengthsIterator,
-				permutationVector,
-				permutationVector.front(),
-				matrix)
-			);
-			vectorOfThreadsInFlight.back()->join();
-
-			//join w¹tków
-			/*
-			for (auto& a : vectorOfThreadsInFlight) {
-				a->join();
-			}*/
-			
-			bool notFinished = 0;
-			do {
-				notFinished = 0;
-				for (auto a : pathLengths) if (a == INT_MAX) {
-					notFinished = 1;
-					break;
-				}
-			} while (notFinished);
-			
-			int place = 0;
-			int currentPlace = 0;
-			int minimum = INT_MAX;
-			for (auto a : pathLengths) {
-				if (a < minimum) {
-					place = currentPlace;
-					minimum = a;
-				}
-				currentPlace++;
+				ordersIterator++;
+				pathLengthsIterator++;
+				previousVertex = permutationVector.front();
 			}
+		} while (std::next_permutation(permutationVector.begin(), permutationVector.end()));
 
-			this->pathLength = minimum;
-			ordersIterator = orders.begin();
-			std::advance(ordersIterator, place);
-			this->vertexOrder = *ordersIterator;
-
-			executionTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - now);
-
-			for (auto& a : vectorOfThreadsInFlight) {
-				delete a;
+		vectorOfThreadsInFlight.push_back(new std::thread(bruteHelperMultithread,
+			&*ordersIterator,
+			&*pathLengthsIterator,
+			permutationVector,
+			permutationVector.front(),
+			matrix)
+		);
+		vectorOfThreadsInFlight.back()->join();
+		
+		// sprawdzenie czy wszystkie w¹tki siê wykona³y
+		bool notFinished = 0;
+		do {
+			notFinished = 0;
+			for (auto a : pathLengths) if (a == INT_MAX) {
+				notFinished = 1;
+				break;
 			}
+		} while (notFinished);
 			
-			std::cout << pathLength << "\n";
-			std::cout << executionTime.count() << "s\n";
-			for (auto a : this->vertexOrder) std::cout << a << " ";
+		// ustalenie najkrótszej drogi
+		int place = 0;
+		int currentPlace = 0;
+		int minimum = INT_MAX;
+		for (auto a : pathLengths) {
+			if (a < minimum) {
+				place = currentPlace;
+				minimum = a;
+			}
+			currentPlace++;
 		}
-		// problemy
-		else {
 
+		this->pathLength = minimum;
+		ordersIterator = orders.begin();
+		std::advance(ordersIterator, place);
+		this->vertexOrder = *ordersIterator;
+
+		this->executionTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - now);
+
+		for (auto& a : vectorOfThreadsInFlight) {
+			delete a;
 		}
+			
+		displayResults();
 	}
 	else {
-		std::vector<int> order;
-		this->pathLength = bruteHelperFunction(&order, matrix);
-		order.push_back(0);
-		order.insert(order.begin(), 0);
-
-		executionTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - now);
-
-		std::cout << pathLength << "\n";
-		std::cout << executionTime.count() << "s\n";
-		for (auto a : order) std::cout << a << " ";
+		this->pathLength = bruteHelperFunction(&this->vertexOrder, matrix);
+		this->executionTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - now);
+		
+		displayResults();
 	}
 }
 
-/*
-* jeœli brak wierzcho³ków, ³¹czymy z startem, zwracamy drogê do startu + wektor z wierzcho³kiem startu
-* inaczej recursive, sprawdzamy odleg³osci, zwracamy najkrótsz¹ drogê + do wektora dodajemy wierzcho³ek najkrótszej drogi
-*/
+void Algorithms::displayResults() {
+	std::cout << "\nDlugosc sciezki: " << pathLength << "\n";
+	std::cout << "Kolejnosc wierzcholkow:\n0 ";
+	for (auto a : this->vertexOrder) std::cout << a << " ";
+	std::cout << "0\nCzas trwania algorytmu: " << executionTime.count() << "s\n";
+}
 
 int Algorithms::bruteHelperFunction(std::vector<int>* orderQueue, Matrix* matrix) {
 	int shortestPath = INT_MAX;
@@ -149,61 +135,9 @@ int Algorithms::bruteHelperFunction(std::vector<int>* orderQueue, Matrix* matrix
 			innerIter = (*outerIter).begin();
 			std::advance(innerIter, previousVertex);
 			currentPath += *innerIter;
-			previousVertex = *permutationIterator;			
-		}
 
-		outerIter = pointerMat->begin();
-		innerIter = (*outerIter).begin();
-		std::advance(innerIter, previousVertex);
-		currentPath += *innerIter;
+			if (currentPath > shortestPath) goto ZA_DUZO;
 
-		if (currentPath < shortestPath) {
-			shortestPath = currentPath;
-			*orderQueue = permutationVector;
-		}
-
-	} while (std::next_permutation(permutationVector.begin(), permutationVector.end()));
-
-	return shortestPath;
-}
-
-void Algorithms::bruteHelperMultithread(std::vector<int>* orderQueue, int* pathLength, std::vector<std::vector<int>> permutations, Matrix* matrix) {
-	int shortestPath = INT_MAX;
-	const int matrixSize = matrix->size;
-	std::vector<std::vector<int>>* pointerMat = &(matrix->mat);
-	std::vector<std::vector<int>>::iterator outerIter = pointerMat->begin(), permIter = permutations.end(); //permutations->end();
-	std::vector<int>::iterator permutationIterator, innerIter = (*outerIter).begin();
-
-	/*
-	for (std::vector<int> a : permutations) {
-		for (int n : a) {
-			std::cout << n;
-		}
-		std::cout << " ";
-	}
-	std::cout << "\n";
-	*/
-
-	// dopóki mamy jakieœ dostarczone permutacje
-	while (permIter != permutations.end()) {
-		permutationIterator = (*permIter).begin();
-
-		int previousVertex = 0;
-		int currentPath = 0;
-		int currentVertexNumber = 0;
-
-		for (permutationIterator = (*permIter).begin(); currentVertexNumber < matrixSize - 1; permutationIterator++, currentVertexNumber++) {
-			/* np pierwsza iteracja(od zrodla)
-			* outerIter = pierwszy wierzcholek permutationVector
-			* inner = previousVertex (czyli zrodlo, czyli 0)
-			*/
-
-			outerIter = pointerMat->begin();
-			std::advance(outerIter, *permutationIterator);
-
-			innerIter = (*outerIter).begin();
-			std::advance(innerIter, previousVertex);
-			currentPath += *innerIter;
 			previousVertex = *permutationIterator;
 		}
 
@@ -212,19 +146,17 @@ void Algorithms::bruteHelperMultithread(std::vector<int>* orderQueue, int* pathL
 		std::advance(innerIter, previousVertex);
 		currentPath += *innerIter;
 
+		ZA_DUZO:
 		if (currentPath < shortestPath) {
 			shortestPath = currentPath;
-			*orderQueue = *permIter;
+			*orderQueue = permutationVector;
 		}
+	} while (std::next_permutation(permutationVector.begin(), permutationVector.end()));
 
-		permIter++;
-	}
-
-	// zamiast return
-	*pathLength = shortestPath;
+	return shortestPath;
 }
 
-void Algorithms::bruteHelperMultithreadNew(std::vector<int>* orderQueue, int* pathLength, std::vector<int> permutation, int permutationNumber, Matrix* matrix) {
+void Algorithms::bruteHelperMultithread(std::vector<int>* orderQueue, int* pathLength, std::vector<int> permutation, int permutationNumber, Matrix* matrix) {
 	int shortestPath = INT_MAX;
 	const int matrixSize = matrix->size;
 	std::vector<std::vector<int>>* pointerMat = &(matrix->mat);
